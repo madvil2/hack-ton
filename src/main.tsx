767d883f879cba87d020942efe7c09fbf274fe75
KONSTANTIN KAIMOV <ikaimov98@gmail.com>
2024-06-01 17:47:36 +0200

    install deps

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import WebApp from '@twa-dev/sdk'
 import './index.css'
+
+WebApp.ready();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
